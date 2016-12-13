be9be5f2765304d87b35ae19250222df05d61c57
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-13 13:52:47 -0300

    ticket:1 added view test docstring

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,7 +4,10 @@ from django.core.urlresolvers import reverse
 
 class ViewsTests(TestCase):
     def test_index(self):
+        """
+        Check index page
+        """
         client = Client()
         response = client.get(reverse('index'))
-        self.assertContains(response,"Clara")
+        self.assertContains(response, "Clara")
         self.assertContains(response, "Contacts")
