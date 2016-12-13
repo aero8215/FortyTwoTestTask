68c3b9b512f9ffa3e37dace079dfa4188d27872e
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-13 13:00:02 -0300

    ticket:1 removed commented code

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,7 +1,5 @@
 # Create your views here.
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    #return HttpResponse("Hola")
     return render(request, 'hello/mockup.html', {})
