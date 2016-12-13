3fa0f4e4dc641da9c1a4332da2ef27b4031ddddb
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-13 12:44:20 -0300

    ticket:1 html mockup built into django

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    #return HttpResponse("Hola")
+    return render(request, 'hello/mockup.html', {})
