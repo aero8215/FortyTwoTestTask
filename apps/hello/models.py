5055057f85c30094f8ab827f79834c42b123fe9e
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-14 16:39:16 -0300

    ticket:1 changed datetime to date type

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -9,7 +9,7 @@ class Information(models.Model):
     skype = models.CharField(max_length=30)
     other_contacts = models.TextField()
     bio = models.TextField()
-    birth_date = models.DateTimeField()
+    birth_date = models.DateField()
 
     def __str__(self):
         return ' '.join([self.first_name, self.last_name])
