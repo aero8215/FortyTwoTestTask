1b3bd8b7a52d04ca1be0d7c63ded9449b23e753c
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-14 08:22:15 -0300

    ticket:1 Added model and model test

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+
+
+class PersonalData(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    jabber = models.CharField(max_length=30)
+    skype = models.CharField(max_length=30)
+    other_contacts = models.TextField()
+    bio = models.TextField()
+    birth_date = models.DateTimeField()
+
+    def __str__(self):
+        return ' '.join([self.first_name, self.last_name])
+
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
-
+from .models import PersonalData
 
 class ViewsTests(TestCase):
     def test_index(self):
@@ -11,3 +11,13 @@ class ViewsTests(TestCase):
         response = client.get(reverse('index'))
         self.assertContains(response, "Clara")
         self.assertContains(response, "Contacts")
+
+
+class PersonalDataTest(TestCase):
+     def setUp(self):
+         PersonalData.objects.create(first_name="Charles", last_name="Chaplin",bio="bla bla bla", birth_date="1990-12-20")
+
+     def test_personal_data(self):
+         obj = PersonalData.objects.get(first_name="Charles")
+         self.assertEqual(obj.first_name, "Charles")
+         self.assertEqual(obj.last_name, "Chaplin")
