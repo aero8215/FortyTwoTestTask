1701af7f28a04de4a44db28a1e1705992c07a668
Clara Ferrando <clara.ferrando@gmail.com>
2016-12-15 23:55:35 -0300

    ticket:1 configured static files

--- fortytwo_test_task/settings.py
+++ fortytwo_test_task/settings.py
@@ -25,9 +25,9 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 SECRET_KEY = 'x=c0_e(onjn^80irdy2c221#)2t^qi&6yrc$31i(&ti*_jf3l8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'getbarista.com',
                  'fortytwotesttask-458.aero8215.at.getbarista.com']
@@ -108,13 +108,18 @@ FIXTURE_DIRS = (
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,5 +11,10 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('apps.hello.urls'))
+    url(r'', include('apps.hello.urls')),
+    url(
+        regex=r'^static/(?P<path>.*)$',
+        view='django.views.static.serve',
+        kwargs={'document_root': settings.STATIC_ROOT, }
+    )
 )
